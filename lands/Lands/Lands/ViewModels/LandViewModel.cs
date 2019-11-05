9493ac0772441fbf25bfbc95555202a3ef4ca268
jerodrig01 <46756137+jerodrig01@users.noreply.github.com>
2019-11-04 19:10:45 -0600

    Curso 15 terminado

--- lands/Lands/Lands/ViewModels/LandViewModel.cs
+++ lands/Lands/Lands/ViewModels/LandViewModel.cs
@@ -1,10 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
+﻿
 namespace Lands.ViewModels
 {
+    using Models;
+
     public class LandViewModel
     {
+        #region Properties
+        public Land Land 
+        { 
+            get;
+            set; 
+        }
+        #endregion
+
+        #region Constructors
+        public LandViewModel(Land land)
+        {
+            this.Land = land;
+        }
+        #endregion
     }
 }
